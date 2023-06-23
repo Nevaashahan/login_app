3a54bb642a1fb1c777842096e2f5275afe299a5c
Tharmabalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2023-06-23 19:51:38 +0530

    Add files via upload
    login app 
    platform: visual studio
    language: C#

---
+++ First_login_app/First_login_app/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_login_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textUserName.Text=="admin"&&textPassword.Text=="123")
+            {
+                new Form2().Show();
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("your username or password is incorrect, pls try again");
+                textUserName.Clear();
+                textPassword.Clear();
+                textUserName.Focus();
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            textUserName.Clear();
+            textPassword.Clear();
+            textUserName.Focus();
+        }
+    }
+}
